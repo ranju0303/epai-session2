f5f0fec75f8e2186649552d7ea0277b52441113c
Abhishek Kumar <abhishekchand.kumar@gmail.com>
2020-07-24 21:35:49 +0530

    Update session2.py

--- session2.py
+++ session2.py
@@ -1,6 +1,7 @@
 from typing import List
 import time
-
+import sys
+import gc
 # Here in this code we will be leaking memory because we are creating cyclic reference. 
 # Find that we are indeed making cyclic references.
 # Eventually memory will be released, but that is currently not happening immediately.
@@ -8,43 +9,36 @@ import time
 # Refer to test_clear_memory Test in test_session2.py to see how we're crudely finding that
 # this code is sub-optimal.
 class Something(object):
-
     def __init__(self):
         super().__init__()
         self.something_new = None
-
-
+    def __repr__(self):
+        return 'Custom Representation:::{0}'.format(self.something_new)
 class SomethingNew(object):
-
     def __init__(self, i: int = 0, something: Something = None):
         super().__init__()
         self.i = i
         self.something = something
-
-
+    def __repr__(self):
+        return 'Custom Representation:::{0}'.format(self.something)
 def add_something(collection: List[Something], i: int):
     something = Something()
     something.something_new = SomethingNew(i, something)
     collection.append(something)
-
-def reserved_Function():
-    # to be used in future if required
+def reserved_function():
     pass
 
 def clear_memory(collection: List[Something]):
-    # you probably need to add some comment here
-
-    
+# you probably need to add some comment here
+    gc.collect()
     collection.clear()
-
-
+    gc.collect()
 def critical_function():
+    #import pdb;pdb.set_trace()
     collection = list()
-    for i in range(1, 1024 * 128):
+    for i in range(1, 1024*128):
         add_something(collection, i)
     clear_memory(collection)
-
-
 # Here we are suboptimally testing whether two strings are exactly same or not
 # After that we are trying to see if we have a particular character in that string or not
 # Currently the code is suboptimal. Write it in such a way that it takes 1/10 the current time
@@ -60,10 +54,15 @@ def compare_strings_old(n):
     for i in range(n):
         if 'd' in char_list:
             pass
-
 # YOU NEED TO CHANGE THIS PROGRAM
 def compare_strings_new(n):
-    time.sleep(6) # remove this line, this is just to simulate your "slow" code
-
-
-
+#time.sleep(1) # remove this line, this is just to simulate your "slow" code
+    a = sys.intern('a long string that is not intered' * 200)
+    b = sys.intern('a long string that is not intered' * 200)
+    for i in range(10000000):
+        if a is b:
+            pass
+    char_list = set(a)
+    for j,i in enumerate(range(10000000)):
+        if 'd' in char_list:
+            break
